c47d53638c2bd11591dabb92edb466c16afff62d
Arthur Kalikiti <arthur.kalikiti@infratel.co.zm>
2023-04-13 15:38:22 +0200

    feat: Added landing page

--- resources/views/livewire/widgets/show-records.blade.php
+++ resources/views/livewire/widgets/show-records.blade.php
@@ -18,9 +18,12 @@
         @foreach ($records as $zone)
             <div wire:loading.remove>
                 <div class="flex flex-row justify-evenly">
+                   
                     <div class="relative z-0 w-1/5 mb-6 group">
-                        <a href="{{route('dash.zone', ['zone' => $zone])}}"><input disabled value="{{$zone}}" type="text" name="host" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/></a>
+                        <a href="{{route('dash.zone', ['zone' => $zone])}}">
+                        <input disabled value="{{$zone}}" type="text" name="host" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                         <label for="host" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Host</label>
+                        </a>
                     </div>
                     <div class="p-6">
                         <div class="ml-auto flex justify-end">
