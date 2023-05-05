4fb74bd59738f55bc3c7bbd28552eba011f58f1c
Arthur Kalikiti <arthur@kalikiti.net>
2023-05-05 23:21:33 +0200

    fix: Visibility of RR drop down options when in dark mode
    feat: Changed buttons styling to use rounded edges

--- resources/views/livewire/widgets/create-record.blade.php
+++ resources/views/livewire/widgets/create-record.blade.php
@@ -14,7 +14,7 @@
         </div>
     
         <div class="relative z-0 mb-6 group">
-            <select wire:model="type" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:bg-transparent dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" ">
+            <select wire:model="type" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer" placeholder=" ">
                 <option selected value="A">A</option>
                 <option value="AAAA">AAAA</option>
                 <option value="TXT">TXT</option>
